4953d3df02d3a44be9dffc22b5ec0ee1e475f05b
Neptun1403 <sahvldovruft@gmail.com>
2026-05-08 02:19:01 -0700

    fix server.js

--- server.js
+++ server.js
@@ -1,34 +1,9 @@
-// const express = require("express");
-// const categoryRoutes = require("./routes/categoryRoutes");
-// const authRoutes = require("./routes/authRoutes");
-// //const productRoutes = require("./routes/productRoutes");
-
-// const app = express();
-// const PORT = 3000;
-
-// app.use(express.json());
-// app.use((req, res, next) => {
-//   console.log("METHOD:", req.method);
-//   console.log("URL:", req.url);
-//   console.log("BODY:", req.body);
-//   next();
-// });
-// app.get("/", (req, res) => {
-//   res.send("E-commerce API is running");
-// });
-
-// app.use("/categories", categoryRoutes);
-// app.use("/", authRoutes);
-// //app.use("/products", productRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
 require('dotenv').config();
 const express = require('express');
 const categoryRoutes = require('./routes/categoryRoutes');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -48,8 +23,9 @@ app.get('/', (req, res) => {
 
 app.use('/categories', categoryRoutes);
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 app.use('/', authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log('Server running on http://localhost:' + PORT);
 });
